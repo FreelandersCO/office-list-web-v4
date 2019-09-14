5a17bf47b4103bb0b649cf56dc0b5a2d8d344fb8
Sebastian Sanabria <sebas.1022@gmail.com>
2019-09-14 01:34:14 -0500

    Bussines list desktop

--- src/app/shared/bc-slider/bc-slider.component.ts
+++ src/app/shared/bc-slider/bc-slider.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+        ViewChild,
+        QueryList,
+        ElementRef,
+        Input,
+        ViewChildren} from '@angular/core';
 
 @Component({
     selector: 'office-list-bc-slider',
     templateUrl: './bc-slider.component.html',
     styleUrls: ['./bc-slider.component.scss']
 })
-export class BcSliderComponent implements OnInit {
+export class BcSliderComponent {
+    // tslint:disable-next-line: no-input-rename
+    @Input('images') images;
+    @ViewChild('slider', { static: false }) slidesContainer: ElementRef<HTMLDivElement>;
+    @ViewChildren('item') items: QueryList<ElementRef<HTMLDivElement>>;
 
-    constructor() { }
+    private slidesIndex = 0;
 
-    ngOnInit() {
+    get currentItem(): ElementRef<HTMLDivElement> {
+        return this.items.find((item, index) => index === this.slidesIndex);
+    }
+
+    onClickLeft() {
+        const width = this.currentItem.nativeElement.offsetWidth;
+        this.slidesContainer.nativeElement.scrollLeft = this.slidesContainer.nativeElement.scrollLeft - width;
+
+        if (this.slidesIndex > 0) {
+            this.slidesIndex--;
+        }
+    }
+
+    onClickRight() {
+        this.slidesContainer.nativeElement.scrollLeft += this.currentItem.nativeElement.offsetWidth;
+
+        if (this.slidesIndex < this.items.length - 1) {
+            this.slidesIndex++;
+        }
     }
 
 }
