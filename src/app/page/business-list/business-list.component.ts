616ba481baae086cf2632fd8694b57edea73641e
Sebastian Sanabria <sebas.1022@gmail.com>
2019-09-15 18:34:36 -0500

    Filter Area

--- src/app/page/business-list/business-list.component.ts
+++ src/app/page/business-list/business-list.component.ts
@@ -10,17 +10,43 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BusinessListComponent implements OnInit {
     bussinesCenters;
+    areas;
+    originalData;
+    bussinesCentersCount;
+    city;
+    state;
 
-    constructor(private api: ApiServicesService,
-                private route: ActivatedRoute) { }
+    constructor(private api: ApiServicesService, private route: ActivatedRoute) { }
 
     ngOnInit() {
-        this.route.params.subscribe( params => {
-            this.api.getBussinesList(params['country'], params['state'], params['city']).subscribe(result =>
-                this.bussinesCenters = result
-            );
-        });
+        this.route.params.subscribe(params => {
+            this.city = params['city'];
+            this.state = params['state'];
+            this.api.getBussinesList(params['country'], params['state'], params['city']).subscribe(result => {
+                this.bussinesCenters = result;
+                this.bussinesCentersCount = Object.keys(this.bussinesCenters).length;
+                this.originalData = result;
+                this.areas = this.bussinesCenters.reduce((a, d) => {
+                    if (a.indexOf(d.area_name) === -1) {
+                        a.push(d.area_name);
+                    }
 
+                    return a;
+                }, []);
+            });
+        });
+    }
+
+    filterArea(area) {
+        if (area !== '') {
+            this.bussinesCenters = this.originalData.filter((item) => {
+                return item.area_name === area;
+            });
+        } else {
+            this.bussinesCenters = this.originalData;
+        }
+
+        this.bussinesCentersCount = Object.keys(this.bussinesCenters).length;
     }
 
 }
