044c785416f309ee3f00345e8fbb028b4cc5cdd4
Lady <ladycastdg@gmail.com>
2019-10-06 21:53:52 -0500

    Actualizacion final

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,state,style,animate,transition,query,animateChild } from '@angular/animations';
 
 @Component({
     selector: 'office-list-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+	styleUrls: ['./header.component.scss'],
+	animations: [
+		trigger('ngIfAnimation', [
+		  transition(':enter, :leave', [
+			query('@*', animateChild())
+		  ])
+		]),
+		trigger('easeIn', [
+		  transition('void => *', [
+			  style({
+				height: 0,
+				opacity: 0,
+			  }),
+			  animate("500ms ease-in", style({
+				height: 100,
+				opacity: 1,
+			  }))
+		  ]),
+		  transition('* => void', [
+			  style({
+				height: 100,
+				opacity: 1,
+			  }),
+			  animate("500ms ease-in", style({
+				height: 0,
+				opacity: 0,
+			  }))
+			])
+		  ])
+	  ]
 })
 export class HeaderComponent implements OnInit {
-    public status = false;
+	public status = false;
+	public optionsNav:boolean = false;
+	public optionsNavShow:any = 'Show';
     constructor() { }
 
     ngOnInit() {
@@ -14,5 +46,14 @@ export class HeaderComponent implements OnInit {
 
     openMenu() {
         this.status = !this.status;
-    }
+	}
+	
+	showOptionsNav() {
+		this.optionsNav = !this.optionsNav;
+	
+		if(this.optionsNav)  
+		  this.optionsNavShow = "Show";
+		else
+		  this.optionsNavShow = "Hide";
+	}
 }
