fbdbf7dd216feac1a6bb89c7e203dfa3553c1039
Sebas Sanabria <freelanders@gmail.com>
2019-10-22 12:45:49 -0500

    Muchos Fix

--- src/app/shared/gallery-detail-office/gallery-detail-office.component.ts
+++ src/app/shared/gallery-detail-office/gallery-detail-office.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, SimpleChange } from '@angular/core';
 
 @Component({
-  selector: 'office-list-gallery-detail-office',
-  templateUrl: './gallery-detail-office.component.html',
-  styleUrls: ['./gallery-detail-office.component.scss']
+	selector: 'office-list-gallery-detail-office',
+	templateUrl: './gallery-detail-office.component.html',
+	styleUrls: ['./gallery-detail-office.component.scss']
 })
-export class GalleryDetailOfficeComponent implements OnInit {
+export class GalleryDetailOfficeComponent implements OnChanges, OnInit {
+	@Input() images;
+	_images;
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+	}
+	ngOnChanges(changes: SimpleChanges) {
+		const images: SimpleChange = changes.images;
+		if (images.currentValue !== null) {
+			this._images = images.currentValue.splice(0,2);
+		}
+	}
 }
