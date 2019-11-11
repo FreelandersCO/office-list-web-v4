1679fd5e31ef7b87f3520730b7555de1742088b9
Lady <ladycastdg@gmail.com>
2019-11-10 23:56:42 -0500

    Resources/AboutUs - Pages

--- src/app/page/page-about/page-about.component.ts
+++ src/app/page/page-about/page-about.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-about.component.scss']
 })
 export class PageAboutComponent implements OnInit {
-
+	public detailInfoTeam = false;
+	public detailInfoTeamg = false;
+	public detailInfoTeama = false;
+	public detailInfoTeamm = false;
+	public detailInfoTeamj = false;
   constructor() { }
 
   ngOnInit() {
   }
+
+  	showModalInformationU() {
+		this.detailInfoTeam = true;
+	}
+	showModalInformationG() {
+		this.detailInfoTeamg = true;
+	}
+	showModalInformationA() {
+		this.detailInfoTeama = true;
+	}
+	showModalInformationM() {
+		this.detailInfoTeamm = true;
+	}
+	showModalInformationJ() {
+		this.detailInfoTeamj = true;
+	}
+
+	closeModal() {
+		this.detailInfoTeam = false;
+	}
+
+	closeModalG() {
+		this.detailInfoTeamg = false;
+	}
+	closeModalA() {
+		this.detailInfoTeama = false;
+	}
+	closeModalM() {
+		this.detailInfoTeamm = false;
+	}
+	closeModalJ() {
+		this.detailInfoTeamj = false;
+	}
 
 }
