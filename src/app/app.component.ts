909eb5f72e9debc91f26a6db6cccebbecc438fb8
Sebas Sanabria <freelanders@gmail.com>
2019-11-16 22:48:42 -0500

    GTM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Router , NavigationEnd} from '@angular/router';
+
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
 	selector: 'office-list-root',
@@ -8,8 +13,19 @@ import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 })
 export class AppComponent implements OnInit {
 
-	constructor(@Inject(PLATFORM_ID) private platformId) {
+	constructor(
+		@Inject(PLATFORM_ID) private platformId,
+		private router: Router) {
+		const navEndEvents$ = this.router.events
+			.pipe(
+				filter(event => event instanceof NavigationEnd)
+			);
 
+		navEndEvents$.subscribe((event: NavigationEnd) => {
+			gtag('config', 'UA-219071-1', {
+				'page_path': event.urlAfterRedirects
+			});
+		});
 	}
 
 	ngOnInit(): void {
