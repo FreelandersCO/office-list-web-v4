126447f452f8a275c31a7e1dbc9bdf2565ae7ffa
Sebas Sanabria <freelanders@gmail.com>
2020-01-16 22:29:39 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	//apiUrl: 'https://www.officelist.com/api'
-	apiUrl: 'http://ol.freelanders.co:8085'
+	apiUrl: 'https://www.officelist.com/api'
+	//apiUrl: 'http://ol.freelanders.co:8085'
 };
